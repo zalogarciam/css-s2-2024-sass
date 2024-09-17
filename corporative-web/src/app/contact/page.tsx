de669ab37fd9ec30a28499c9fbee4aa691660998
zalogarciam <gegarciam95@gmail.com>
2024-09-16 21:15:56 -0500

    Upload structure of corp web with sass

---
+++ corporative-web/src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+const ContactPage = () => {
+  return (
+    <div>
+      <h1 className="primary-title">Contacto</h1>
+    </div>
+  );
+};
+
+export default ContactPage;
---
+++ corporative-web/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1 className="primary-title">This is a corp Web</h1>
+        <button className="primary-btn">Aceptar</button>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
