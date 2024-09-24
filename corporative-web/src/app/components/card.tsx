3f66220da8ca7f27488953dba4e3cbc792023390
zalogarciam <gegarciam95@gmail.com>
2024-09-23 21:28:43 -0500

    Added mixins examples

--- corporative-web/src/app/components/card.tsx
+++ corporative-web/src/app/components/card.tsx
@@ -3,9 +3,17 @@ const Card = ({ name, description }: any) => {
   return (
     <div className={styles.profileCard}>
       <div className={styles.header}>{name}</div>
-      <div className={styles.description}>{description}</div>
+      <div className={styles.description}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+        odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
+        quis sem at nibh elementum imper Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante
+        dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imper Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+        Praesent libero.
+      </div>
       <div className={styles.footer}>
-        <button className={styles.button}>Aceptar</button>
+        <button className={styles.button}>See more ...</button>
       </div>
     </div>
   );
--- corporative-web/src/app/page.tsx
+++ corporative-web/src/app/page.tsx
@@ -1,16 +1,35 @@
 import Image from "next/image";
 import Card from "./components/card";
-
+import styles from "@/app/styles/layout.module.scss";
 export default function Home() {
   return (
     <div className="container mx-auto">
       <main>
-        <h1 className="primary-title">This is a corp Web</h1>
-        <div>
-          <Card name="Enrique" description="?"></Card>
-          <Card name="Gonzalo" description="..."></Card>
+        <nav>
+          <ul className={styles.navBar}>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+        <h1 className="primary-title">Macrosoft</h1>
+        <div className="primary-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+          odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
+          quis sem at nibh elementum imper Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed
+          cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum
+          imper Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+          Nulla quis sem at nibh elementum imper
+        </div>{" "}
+        <h1 className="primary-subtitle">Our Culture</h1>
+        <div className={styles.card}>
+          <Card name="Mission"></Card>
+          <Card name="Vision"></Card>
+          <Card name="Values"></Card>
         </div>
-        <button className="primary-btn">Aceptar</button>
       </main>
       <footer></footer>
     </div>
