76b387070e73205edaecb4f91e29ebea43ded1e2
zalogarciam <gegarciam95@gmail.com>
2024-11-25 19:34:25 -0500

    Fixing text

--- corporative-web/src/app/page.tsx
+++ corporative-web/src/app/page.tsx
@@ -5,27 +5,48 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="container mx-auto">
-      <main>
-       
-        <h1 className="primary-title">MACROSOFT</h1>
-        <div className="primary-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
-          odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
-          quis sem at nibh elementum imper Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed
-          cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum
-          imper Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-          Nulla quis sem at nibh elementum imper
-        </div>{" "}
-        <h1 className="primary-subtitle">Our Culture</h1>
-        <div className={styles.card}>
-          <Card name={"Mission"}></Card>
-          <Card name="Vision"></Card>
-          <Card name="Values"></Card>
-        </div>
-      </main>
-      <footer></footer>
+      <h1 className="primary-title">MACROSOFT</h1>
+      <div className="primary-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+        odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
+        quis sem at nibh elementum imper Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante
+        dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imper Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+        Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem
+        at nibh elementum imper
+      </div>{" "}
+      <h1 className="primary-subtitle">Our Culture</h1>
+      <div className={styles.card}>
+        <Card name={"Mission"}></Card>
+        <Card name="Vision"></Card>
+        <Card name="Values"></Card>
+      </div>
+      <div className="primary-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+        odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
+        quis sem at nibh elementum imper Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante
+        dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imper Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+        Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem
+        at nibh elementum imper Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante
+        dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imper Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+        Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem
+        at nibh elementum imper Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante
+        dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imper Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+        Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem
+        at nibh elementum imper Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante
+        dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imper Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+        Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem
+        at nibh elementum imper
+      </div>{" "}
     </div>
   );
 }
